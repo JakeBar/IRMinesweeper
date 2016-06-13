e7c5d2ac804d24fbb0cc5db8ccdd64e9e4434d71
JakeBar <ortin666@hotmail.com>
2016-06-13 17:31:20 +1000

    Display Randomly Generated Mines

--- Minesweeper.rb
+++ Minesweeper.rb
@@ -3,18 +3,46 @@
 # email - s3380519@student.rmit.edu.au
 
 class Board
-	attr_accessor :row_count, :column_count
-	def initialize (row_count, column_count)
+	attr_accessor :row_count, :column_count, :mines
+	
+	def initialize (row_count, column_count, mines)
+		@width = column_count
+		@height = row_count
+		@mines = mines
+		@squares = []
+
+		randomise_minefield
+		display_minefield
 	end
 
-	def add_mines
+	def randomise_minefield
+		(0...@width).each do |x|
+			(0...@height).each do |y|
+				@squares[x] = [] unless @squares[x]
+				@squares[x][y] = Square.new(x,y)
+			end
+		end
+
+		mine_count = 0
+
+		(0...@mines).each do |count|
+			x = rand(@width)
+			y = rand(@height)
+			puts "#{}"
+			square = @squares[x][y]
+			if square.mine == false
+				square.mine = true
+				mine_count += 1
+			end
+		end
 	end
 
 	def display_minefield
 		puts "\nPrinting minefield\n"
-		field.each do |row|
-			row.each do |column|
-				if (column.mine == true)
+		(0...@width).each do |x|
+			(0...@height).each do |y|
+				square = @squares[x][y]
+				if (square.mine == true)
 					print "* "
 				else
 					print "0 "
@@ -28,10 +56,10 @@ end
 class Square
 	attr_accessor :x, :y, :mine
 
-	def initialize (x, y, mine)
+	def initialize (x, y)
 		@x = x
 		@y = y
-		@mine = Mine.new()
+		@mine = false
 	end
 end
 
@@ -41,52 +69,20 @@ end
 def prompt_user
 
 	# user input
-	print "Please enter lines and columns: "
+	print "Please enter lines, columns and number of mines: "
 
 	input = gets.chomp
 	dimensions = input.split(" ")
 
 	row_count = dimensions[0].to_i
 	column_count = dimensions[1].to_i
+	mine_count = dimensions[2].to_i
 
-	puts "Input received. #{row_count} row(s), #{column_count} column(s)"
+	puts "Input received. #{row_count} row(s), #{column_count} column(s), #{mine_count} mine(s)"
 
-	i = 0
-
-	# create new board
-	minesweeper = Board.new(row_count, column_count)
-
-	# must create an array of objects to hold information
-	field = Array.new
-
-	while i < row_count do
-		i += 1
-
-		row = []
-
-		print "Input row number #{i} values: "
-		input = gets.chomp
-		temp_row = input.split(" ")
-		mine_state = false
-		
-		j = 0
-
-		while j < column_count
-			if temp_row[j] == "*"
-				mine_state = true
-			else
-				mine_state = false
-			end
-			row << Square.new(i,j, mine_state)
-			j += 1
-		end
-
-		field << row
-
-	end	
-
+	minesweeper = Board.new(row_count, column_count, mine_count)
 end
 
 prompt_user
 
-#Board.display_minefield
+
